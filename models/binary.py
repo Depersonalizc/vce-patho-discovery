2592506c53e365993087da98bb0b7c7c9cf6b4b8
FuFu <82163913+fqzz2000@users.noreply.github.com>
2022-04-08 01:03:15 +0800

    Add DenseNet

--- models/binary.py
+++ models/binary.py
@@ -26,3 +26,28 @@ def ResNetBinary(backbone='resnet152', num_hidden=512, pretrained=True):
     )
 
     return model
+
+def DenseNetBinary(backbone='resnet152', num_hidden=512, pretrained=True):
+    densenet = getattr(models, backbone)(pretrained)
+    in_features = densenet.classifier.in_features
+
+    head = nn.Sequential(
+        nn.AdaptiveAvgPool2d(1),
+        nn.Flatten(),
+
+        nn.BatchNorm1d(in_features),
+        nn.Dropout(0.5),
+        nn.Linear(in_features=in_features, 
+                  out_features=num_hidden),
+        nn.ReLU(),
+
+        nn.BatchNorm1d(num_hidden),
+        nn.Dropout(0.5),
+        nn.Linear(in_features=num_hidden, out_features=2),
+    )
+    model = nn.Sequential(
+        nn.Sequential(*list(densenet.children())[:-2]),
+        head
+    )
+
+    return model

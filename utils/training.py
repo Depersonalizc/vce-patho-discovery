891c1b09269010461a913afb924594d23cdc3e6f
Depersonalizc <1046770165@qq.com>
2022-05-04 19:07:24 +0000

    yee

--- utils/training.py
+++ utils/training.py
@@ -13,6 +13,7 @@ def poly_lr_scheduler(optimizer, base_lr, max_iter, power=0.9, **kwargs):
     lr_lambda = lambda it: base_lr * ((1 - it / max_iter) ** power)
     return torch.optim.lr_scheduler.LambdaLR(optimizer, [lr_lambda], **kwargs)
 
+
 class SelfInformation(nn.Module):
     def __init__(self, eps=1e-30):
         super().__init__()
@@ -22,13 +23,17 @@ class SelfInformation(nn.Module):
         x = x + self.eps
         return -x * torch.log(x)
 
+
 class BinaryEntropy(nn.Module):
-    def __init__(self, eps=1e-30):
+    def __init__(self, eps=1e-30, mean_reduce=True):
         super().__init__()
         self.info = SelfInformation(eps)
+        self.mean_reduce = mean_reduce
 
     def forward(self, p):
         # p: probability map whose elements are from 0 to 1
         entropy = self.info(p) + self.info(1 - p)
-        return entropy.mean()
+        if self.mean_reduce:
+            return entropy.mean()
+        return entropy
 
